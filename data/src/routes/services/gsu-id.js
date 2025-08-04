1e557ca9b70e1ba4fb618c9f7cb325302a7c446d
kosinix <kosinix@users.noreply.github.com>
2025-08-04 14:16:06 +0800

    Avoid uid conflict on creation
    Remove undefined middlename

--- data/src/routes/services/gsu-id.js
+++ data/src/routes/services/gsu-id.js
@@ -57,11 +57,12 @@ router.post('/services/gsu-id', async (req, res, next) => {
 
 
         }
-        // return res.send(data)
         data.idNumber = `${data.idNumber}`.trim()
         data.firstName = `${data.firstName}`.trim()
-        data.middleName = `${data.middleName}`.trim()
+        data.middleName = `${data.middleName ?? ''}`.trim()
         data.lastName = `${data.lastName}`.trim()
+
+        // return res.send(data)
 
         let found = await req.app.locals.db.models.Gsuid.findOne({
             where: {
@@ -76,27 +77,42 @@ router.post('/services/gsu-id', async (req, res, next) => {
                 }
             }
         })
-        if(found){
+        if (found) {
             throw new Error(`You have already applied. Please visit the ICTU.`)
         }
 
+        let refNum = `${passwordMan.genPasscode(4)}`
+        let conflict = null;
+        do {
+            conflict = await req.app.locals.db.models.Gsuid.findOne({
+                where: {
+                    uid: refNum
+                }
+            });
+
+            if (conflict) {
+                // generate a new refNum if conflict exists
+                refNum = `${passwordMan.genPasscode(4)}`
+            }
+        } while (conflict);
+
         let gsuid = req.app.locals.db.models.Gsuid.build({
-            uid: `${passwordMan.genPasscode(4)}`,
+            uid: refNum,
             accountType: data.accountType,
-            idNumber:  data.idNumber,
-            course:  data.course,
-            firstName:  data.firstName,
-            middleName:  data.middleName,
-            lastName:  data.lastName,
-            suffix:  data.suffix,
-            gender:  data.gender,
-            bloodType:  data.bloodType,
-            birthDate:  data.birthDate,
-            email:  data.email,
-            mobileNumber:  data.mobileNumber,
-            guardianName:  data.guardianName,
-            guardianNumber:  data.guardianNumber,
-            address:  data.address,
+            idNumber: data.idNumber,
+            course: data.course,
+            firstName: data.firstName,
+            middleName: data.middleName,
+            lastName: data.lastName,
+            suffix: data.suffix,
+            gender: data.gender,
+            bloodType: data.bloodType,
+            birthDate: data.birthDate,
+            email: data.email,
+            mobileNumber: data.mobileNumber,
+            guardianName: data.guardianName,
+            guardianNumber: data.guardianNumber,
+            address: data.address,
         })
         await gsuid.save()
 
@@ -114,7 +130,7 @@ router.get('/services/gsu-id/transaction/:gsuidId', async (req, res, next) => {
                 uid: req.params.gsuidId
             }
         })
-        if(!gsuid){
+        if (!gsuid) {
             throw new Error(`Transaction not found.`)
         }
         let data = {
@@ -139,7 +155,7 @@ router.get('/services/gsu-id/thanks', async (req, res, next) => {
 router.get('/services/gsu-id', async (req, res, next) => {
     try {
         let data = {}
-        res.locals.title ='ID Services - GSU ID'
+        res.locals.title = 'ID Services - GSU ID'
         res.render('services/gsu-id/create.html', data);
     } catch (err) {
         next(err);
@@ -194,19 +210,19 @@ router.post('/services/gsu-id', async (req, res, next) => {
                 }
             }
         })
-        if(found){
+        if (found) {
             throw new Error(`You have already applied. Please visit the ICTU.`)
         }
 
         let gsuid = req.app.locals.db.models.Gsuid.build({
             uid: `${passwordMan.genPasscode(4)}`,
             accountType: data.accountType,
-            idNumber:  data.idNumber,
-            firstName:  data.firstName,
-            middleName:  data.middleName,
-            lastName:  data.lastName,
-            email:  data.email,
-            mobileNumber:  data.mobileNumber,
+            idNumber: data.idNumber,
+            firstName: data.firstName,
+            middleName: data.middleName,
+            lastName: data.lastName,
+            email: data.email,
+            mobileNumber: data.mobileNumber,
         })
         await gsuid.save()
 
@@ -224,7 +240,7 @@ router.get('/services/gsu-id/transaction/:gsuidId', async (req, res, next) => {
                 uid: req.params.gsuidId
             }
         })
-        if(!gsuid){
+        if (!gsuid) {
             throw new Error(`Transaction not found.`)
         }
         let data = {
